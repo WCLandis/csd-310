6d696575acc83ef46ce8c63b9b2c8c70aa9c4ef3
WCLandis <95384790+WCLandis@users.noreply.github.com>
2022-09-10 12:47:23 -0700

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,5 +1,9 @@
 from pymongo import MongoClient
-url= "mongodb+srv://admin:admin@cluster0.yhxm0bp.mongodb.net/?retryWrites=true&w=majority";
-client= MongoClient(url)
-db=client.pytech
+url= "mongodb+srv://admin:admin@cluster0.yhxm0bp.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(url)
+db = client.pytech
+
+print("\n -- Pytech COllection List --")
 print(db.list_collection_names())
+
+input("\n\n  End of program, press any key to exit... ")
