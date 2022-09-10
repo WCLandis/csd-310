973fb198c98c505db0455f95c4fa203351d7542d
WCLandis <95384790+WCLandis@users.noreply.github.com>
2022-09-10 12:02:35 -0700

    Update pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,11 +1,36 @@
-import pymongo
 from pymongo import MongoClient
-client= MongoClient("mongodb+srv://admin:admin@cluster0.rluwz.mongodb.net/?retryWrites=true&w=majority")
-db= client["pytech"]
-collection = db["students"]
+url= "mongodb+srv://admin:admin@cluster0.yhxm0bp.mongodb.net/?retryWrites=true&w=majority";
+client= MongoClient(url)
+db=client.pytech
 
-post1 = {"_id": 1007, "First Name": "Bill", "Last Name": "Gates"}
-post2 = {"_id": 1008, "First Name": "James", "Last Name": "Hetfield"}
-post3 = {"_id": 1009, "First Name": "William", "Last Name": "Landis"}
+Bill = {
+    "student_id": "1007",
+    "first_name": "Bill",
+    "last_name": "Gates"
+}
 
-collection.insert_many([post1, post2, post3])
+Steve = {
+    "student_id": "1008",
+    "first_name": "Steve",
+    "last_name": "Jobs"
+}
+
+William = {
+    "student_id": "1009",
+    "first_name": "William",
+    "last_name": "Landis"
+}
+
+students= db.students
+
+print("\n  -- INSERT STATEMENTS --")
+Bill_student_id = students.insert_one(Bill).inserted_id
+print("  Inserted student record Thorin Oakenshield into the students collection with document_id " + str(Bill_student_id))
+
+Steve_student_id = students.insert_one(Steve).inserted_id
+print("  Inserted student record Bilbo Baggins into the students collection with document_id " + str(Steve_student_id))
+
+William_student_id = students.insert_one(William).inserted_id
+print("  Inserted student record Frodo Baggins into the students collection with document_id " + str(William_student_id))
+
+input("\n\n  End of program, press any key to exit... ")
